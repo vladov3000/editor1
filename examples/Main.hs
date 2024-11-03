33d4e4f8b738777267daf345119f4251b94389c1
Vladimir Ovechkin <vladov3000@gmail.com>
2024-11-03 00:54:54 -0700

    Add example files.

---
+++ examples/Main.hs
@@ -0,0 +1,15 @@
+module Main where
+
+-- This is a single line comment.
+
+{-
+
+  This is a multi-line comment.
+
+-}
+
+main :: IO ()
+main = putStrLn "Hello World"
+
+two :: Integer
+two = 1 + 1
---
+++ examples/main.c
@@ -0,0 +1,15 @@
+// This is a single line comment.
+
+/*
+
+  This is a multi-line comment.
+
+ */
+
+extern int puts(char* string);
+
+int main() {
+  puts("Hello World");
+}
+
+static int two = 1 + 1;
